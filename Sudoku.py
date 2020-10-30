71a15c334e8c08c2950b9bd0995d8072cdbc717b
Leslie Wen <leslie.wen@gmail.com>
2020-10-30 10:14:51 -0400

    0.00.02

--- Sudoku.py
+++ Sudoku.py
@@ -1,18 +1,10 @@
+from helpers import boardPrinter
+
 #input: 9 x 9 Array of Arrays representing sudoku board 0-9, 0 representing blank
 
-#helper functions:
-#getRow()
-#getColumn()
-#getSquare()
-#solved()
-#boardPrinter()
-
-
-#brute force
+#Brute force
 #Recursive
 
-farr=[[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10]]
-def boardPrinter(arr):
-    for i in range(len(arr)):
-        print(arr[i])
+farr=[[1,2,3,4,5,6,7,'b',9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10]]
+
 boardPrinter(farr)
---
+++ helpers.py
@@ -0,0 +1,12 @@
+def boardPrinter(arr):
+    for i in range(len(arr)):
+        print(arr[i])
+
+
+
+#helper functions:
+#getRow()
+#getColumn()
+#getSquare()
+#solved()
+#boardPrinter()
