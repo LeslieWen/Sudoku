36eb2e9cf807a893fa9da14be366438fca150c80
Leslie Wen <leslie.wen@gmail.com>
2020-10-30 12:09:30 -0400

    0.00.02

--- Sudoku.py
+++ Sudoku.py
@@ -1,10 +1,18 @@
 from helpers import boardPrinter
+from helpers import getRow
+from helpers import getColumn
 
 #input: 9 x 9 Array of Arrays representing sudoku board 0-9, 0 representing blank
 
 #Brute force
 #Recursive
 
+
+
+
+
 farr=[[1,2,3,4,5,6,7,'b',9,10],[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9,10]]
 
+getColumn(farr,2)
+print("")
 boardPrinter(farr)
--- helpers.py
+++ helpers.py
@@ -1,8 +1,17 @@
 def boardPrinter(arr):
     for i in range(len(arr)):
         print(arr[i])
+def getRow(arr,rowNum):
+    return(arr[rowNum])
 
+def printRow():
 
+def getColumn(arr,colNum):
+    colArr=[]
+    for i in range(len(arr)):
+        colArr.append(arr[i][colNum])
+    for i in range(len(colArr)):
+        print(colArr[i])
 
 #helper functions:
 #getRow()
