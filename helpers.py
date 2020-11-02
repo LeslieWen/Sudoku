31683b8ebc63f50c3c01ae2462a2aeb5622c63f0
Leslie Wen <leslie.wen@gmail.com>
2020-11-02 11:03:34 -0500

    0.00.2 Refactoring helpers.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,19 @@
 #need to refactor boardPrinter to be prettier, also print grid
 def boardPrinter(arr):
+    print("|=======|=======|=======|")
     for i in range(len(arr)):
-        print(arr[i])
+        tempStr="| "
+        for j in range(len(arr[i])):
+            if arr[i][j] != "b":
+                tempStr=tempStr+str(arr[i][j])
+            else:
+                tempStr=tempStr+". "
+        tempStr=tempStr+" |"
+        print(tempStr)
+    print("|=======|=======|=======|")
+
+
+
 def getRow(arr,rowNum):
     return(arr[rowNum])
 def getColumn(arr,colNum):
