b7571706294bfcb90bf6541184be25c300e83d7e
Leslie Wen <leslie.wen@gmail.com>
2020-11-04 11:37:24 -0500

    working on solved function

--- helpers.py
+++ helpers.py
@@ -16,14 +16,11 @@ def boardPrinter(arr):
                 else:
                     tempStr=tempStr+". "
         print(tempStr)
-
     print("|=======|=======|=======|")
-
 def getRow(arr,rowNum):
     return(arr[rowNum-1])
 def printRow(arr,rowNum):
     print(getRow(arr,rowNum))
-
 def getColumn(arr,colNum):
     colArr=[]
     for i in range(len(arr)):
@@ -31,9 +28,7 @@ def getColumn(arr,colNum):
     return(colArr)
 def printColumn(arr,colNum):
     print(getColumn(arr,colNum))
-
 #input two coordinate pairs row and col
-
 def getSquare(row,col,arr):
     row=row-1
     col=col-1
@@ -44,7 +39,6 @@ def getSquare(row,col,arr):
         squareArr=arr[3:6]
     elif(6<=row<=8):
         squareArr=arr[6:9]
-
     if (0<=col<=2):    
         for i in range(len(squareArr)):
             squareArr[i]=(squareArr[i][:3])
@@ -56,5 +50,40 @@ def getSquare(row,col,arr):
             squareArr[i]=(squareArr[i][6:9])
     return(squareArr)
 
-def solved():
-    print("placeholder")
+def printSquare(row,col,arr):
+    content=getSquare(row,col,arr)
+    for i in range(len(content)):
+        for j in range(len(content[i])):
+            if content[i][j]=="b":
+                content[i][j]="."
+    print("|=======|")
+    print("| "+str(content[0][0])+" "+str(content[0][1])+" "+str(content[0][2])+" |")
+    print("| "+str(content[1][0])+" "+str(content[1][1])+" "+str(content[2][2])+" |")
+    print("| "+str(content[2][0])+" "+str(content[2][1])+" "+str(content[2][2])+" |")
+    print("|=======|")  
+
+#list of list array flattener
+def arrayFlattener(arr):
+    for i in range(arr):
+
+
+def solved(arr):
+    flag=False
+    tempArr=[]
+    for i in range(len(arr)):
+        if "b" in arr[i]:
+            return flag
+        elif arr[i].sort()!=[1,2,3,4,5,6,7,8,9]:
+            return flag
+    tempArr=getSquare(2,2,arr)
+    tempArr=getSquare(2,5,arr)
+    tempArr=getSquare(2,8,arr)
+
+    tempArr=getSquare(2,2,arr)
+    tempArr=getSquare(2,5,arr)
+    tempArr=getSquare(2,8,arr)
+
+        tempArr=getSquare(2,2,arr)
+    tempArr=getSquare(2,5,arr)
+    tempArr=getSquare(2,8,arr)
+ 
