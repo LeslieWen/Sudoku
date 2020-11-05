1d23bb7b9a7fb8bea7264cbdb3f19956c9e030a0
Leslie Wen <leslie.wen@gmail.com>
2020-11-05 11:44:57 -0500

    Tested helpers

--- Sudoku.py
+++ Sudoku.py
@@ -24,9 +24,21 @@ farr2=[
 [1,2,3,4,5,6,7,8,9],
 [1,2,3,4,5,6,7,8,9]]
 
-boardPrinter(farr2)
+farr3=[
+[8,2,7,1,5,4,3,9,6],
+[9,6,5,3,2,7,1,4,8],
+[3,4,1,6,8,9,7,5,2],
+[5,9,3,4,6,8,2,7,1],
+[4,7,2,5,1,3,6,8,9],
+[6,1,8,9,7,2,4,3,5],
+[7,8,6,2,3,5,9,1,4],
+[1,5,4,7,9,6,8,2,3],
+[2,3,9,8,4,1,5,6,7]]
+
+
+boardPrinter(farr3)
 #print("\n")
 #input two coordinate pairs row num [1-9] and col num [1-9]
-#printSquare(1,5,farr)
+printSquare(1,5,farr3)
 #print(solved(farr))
-print(solved(farr2))
+print(solved(farr3))
