77934f62d70c14f16500cc2e60ff1e5aee1d74e8
Leslie Wen <leslie.wen@gmail.com>
2020-11-14 09:51:34 -0500

    ver 0.00.7

--- Sudoku.py
+++ Sudoku.py
@@ -1,15 +1,2 @@
 from testers import *
-import PySimpleGUI as sg
-layout = [[sg.Text("Cringe, you're gonna lose subscriber")], [sg.Button("OK")]]
-
-# Create the window
-window = sg.Window("NOTICE", layout)
-
-# Create an event loop
-while True:
-    event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
-        break
-window.close()
+test1()
